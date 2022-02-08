407c8873f309059b9e9ae360c95ce458ddf31bb4
Donald Geddes <hi@donaldgeddes.ca>
2022-02-08 15:53:53 -0800

    Add join

---
+++ src/join.js
@@ -0,0 +1,17 @@
+import { pathToFileURL } from 'url'
+
+/**
+ * @param {string} base Base path with leading forward-slash, `/`
+ * @param  {...string} paths One or more paths (leading forward-slash, `/`, optional)
+ * @returns {string} A correctly concatted path
+ * @example
+ *   import { dirname, join } from '@hbauer/convenience-functions'
+ *   const dir = dirname(import.meta.url)
+ *   const pathToFile = join(dir, 'module.js')
+ */
+export const join = (base, ...paths) =>
+  paths.reduce(
+    (url, path) =>
+      new URL(path.slice(0, 1) === '/' ? path.slice(1) : path, url),
+    pathToFileURL(base)
+  ).pathname
