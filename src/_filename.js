5facfa8128834699c99561b1e5985f7e67e2c013
Donald Geddes <hi@donaldgeddes.ca>
2022-02-08 15:53:26 -0800

    Add _filename

---
+++ src/_filename.js
@@ -0,0 +1,11 @@
+import { fileURLToPath } from 'url'
+
+/**
+ * @param {string} fileURL i.e. import.meta.url
+ * @returns {string} The path to the caller's file
+ * @example
+ *   const filename = _filename(import.meta.url) // = /path/to/caller/file.js
+ */
+const _filename = fileURL => fileURLToPath(fileURL)
+
+export { _filename }
