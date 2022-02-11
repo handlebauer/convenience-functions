f990bb4450dd8b5ffc76ba5b67c82dd2cc44e9e0
Donald Geddes <hi@donaldgeddes.ca>
2022-02-10 20:49:59 -0800

    Fix example

--- src/type-of.js
+++ src/type-of.js
@@ -3,7 +3,7 @@
  * @param {any} operand The value to be checked against
  * @returns {string} A string indicating the type, e.g. 'undefined'
  * @example
- *  if (typeOf(randomValue) === 'string') {
+ *  if (typeOf(randomValue) !== 'string') {
  *    console.log("randomValue isn't a string")
  *    console.log(`it's a ${typeOf(randomValue)}`)
  *  }
