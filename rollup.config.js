4c942dd9b6f1d30e8ea6ebff6c74c7959095135d
Donald Geddes <hi@donaldgeddes.ca>
2022-03-01 12:08:45 -0800

    Remove `url` as rollup external (now unnecessary)

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,6 @@ const input = './src/index.js'
 
 export default {
   input,
-  external: ['url'],
   output: [
     { file: pkg.exports.require, format: 'cjs' },
     { file: pkg.exports.import, format: 'esm' },
