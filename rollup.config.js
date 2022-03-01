6ba04c3ce8f91c13dd4e4cfbcaea4510b27132ef
Donald Geddes <hi@donaldgeddes.ca>
2022-03-01 11:52:53 -0800

    Remove browser build

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,12 @@
 import pkg from './package.json'
-import resolve from '@rollup/plugin-node-resolve'
 
 const input = './src/index.js'
 
-export default [
-  {
-    input,
-    output: {
-      file: pkg.exports.browser,
-      format: 'iife',
-    },
-    plugins: [resolve({ preferBuiltins: false, browser: true })],
-  },
-  {
-    input,
-    external: ['url'],
-    output: [
-      { file: pkg.exports.require, format: 'cjs' },
-      { file: pkg.exports.import, format: 'esm' },
-    ],
-  },
-]
+export default {
+  input,
+  external: ['url'],
+  output: [
+    { file: pkg.exports.require, format: 'cjs' },
+    { file: pkg.exports.import, format: 'esm' },
+  ],
+}
