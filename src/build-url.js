f9d866231469d042d74c18ebf50c664f451c40da
Donald Geddes <hi@donaldgeddes.ca>
2022-03-01 11:48:13 -0800

    Add dummy import of `fileURLToPath to aid in shim testing

--- src/build-url.js
+++ src/build-url.js
@@ -1,4 +1,5 @@
 import { pipe } from './fns.js'
+import { fileURLToPath } from 'url'
 
 /**
  * @param {Record<string, string>} params
@@ -26,7 +27,9 @@ const constructURL = ({ host, path }) =>
  *     params: { hello: 'world' },
  *   })
  */
-const buildURL = ({ host, path, params }) =>
-  pipe({ host, path }, constructURL, addParams(params))
+const buildURL = ({ host, path, params }) => {
+  console.log(fileURLToPath('file:'))
+  return pipe({ host, path }, constructURL, addParams(params))
+}
 
 export { buildURL }
