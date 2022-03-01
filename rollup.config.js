e0d34e356cbe124d60075a0fd97c22a7288f512a
Donald Geddes <hi@donaldgeddes.ca>
2022-03-01 11:33:31 -0800

    Add browser build to support shim for `url`

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,23 @@
 import pkg from './package.json'
+import resolve from '@rollup/plugin-node-resolve'
 
 const input = './src/index.js'
 
-export default {
-  input,
-  external: ['url'],
-  output: [
-    { file: pkg.exports.require, format: 'cjs' },
-    { file: pkg.exports.import, format: 'esm' },
-  ],
-}
+export default [
+  {
+    input,
+    output: {
+      file: pkg.exports.browser,
+      format: 'iife',
+    },
+    plugins: [resolve({ preferBuiltins: false, browser: true })],
+  },
+  {
+    input,
+    external: ['url'],
+    output: [
+      { file: pkg.exports.require, format: 'cjs' },
+      { file: pkg.exports.import, format: 'esm' },
+    ],
+  },
+]
--- shims/url/index.js
+++ shims/url/index.js
@@ -2,11 +2,10 @@ const UnsupportedEnvironmentError = new Error(
   'Unsupported operation: this function can only be run in NodeJS'
 )
 
-export const url = {
-  fileURLToPath: () => {
-    throw new UnsupportedEnvironmentError()
-  },
-  pathToFileURL: () => {
-    throw new UnsupportedEnvironmentError()
-  },
+export const fileURLToPath = () => {
+  throw new UnsupportedEnvironmentError()
+}
+
+export const pathToFileURL = () => {
+  throw new UnsupportedEnvironmentError()
 }
