f6ba568746a44b97eef2ac3c20504c97073971c0
Donald Geddes <hi@donaldgeddes.ca>
2023-04-01 17:53:07 -0700

    Add forgotten export

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ export { _dirname } from './_dirname.js'
 export { _filename } from './_filename.js'
 export { join } from './join.js'
 
+export { writeFileHere } from './write-file-here.js'
+
 export { buildURL } from './build-url.js'
 
 export { typeOf } from './type-of.js'
