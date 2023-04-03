6d58dff1a35cfb7fd80a52adf6c97dcbeeb0e8e1
Donald Geddes <hi@donaldgeddes.ca>
2023-04-03 10:33:29 -0700

    Export shallowEqualArray fns

--- src/index.js
+++ src/index.js
@@ -8,4 +8,7 @@ export { buildURL } from './build-url.js'
 
 export { typeOf } from './type-of.js'
 
+export { shallowEqualSortedArrays } from './shallow-equal-sorted-arrays.js'
+export { shallowEqualUnsortedArrays } from './shallow-equal-unsorted-arrays.js'
+
 export * from './fns.js'
