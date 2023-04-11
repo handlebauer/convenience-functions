e33f7c31385babe0986cbe7ef48cbfa2dd733af0
Donald Geddes <hi@donaldgeddes.ca>
2023-04-10 22:18:29 -0700

    Update `titleize` to handle novel case

--- src/titleize.js
+++ src/titleize.js
@@ -2,10 +2,11 @@
  *
  * @param {string} string
  * @example
- *   titelize('hello world') // 'Hello World'
+ *   titleize('hello world') // 'Hello World'
+ * 	 titleize('HELLO WROLD') // 'Hello World'
  */
 export const titleize = string =>
   string
     .split(' ')
-    .map(s => s[0].toUpperCase() + s.slice(1))
+    .map(s => s[0].toUpperCase() + s.slice(1).toLowerCase())
     .join(' ')
