66a410d20db5106a960bc0227fa2dab1a5945a3c
Donald Geddes <hi@donaldgeddes.ca>
2023-04-11 04:23:57 -0700

    Remove `writeFileHere` (useless 😔😔😔)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ export { _dirname } from './_dirname.js'
 export { _filename } from './_filename.js'
 export { join } from './join.js'
 
-export { writeFileHere } from './write-file-here.js'
-
 export { buildURL } from './build-url.js'
 
 export { titleize } from './titleize.js'
