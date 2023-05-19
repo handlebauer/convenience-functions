8014e467abefc2ae7a3d731de896414163130d50
Donald Geddes <hi@donaldgeddes.ca>
2023-05-19 02:06:00 -0700

    Improving typings

--- src/plural.js
+++ src/plural.js
@@ -1,8 +1,18 @@
 /**
- * @param {string} string
- * @esxample
+ * Pluralize a string (note: types are fudged but work)
+ *
+ * @template {string} S
+ * @param {S} string
+ * @returns {S extends `${infer S}s` ? `${S}s` : `${S}s`}
+ * @example
  *   plural('dragon') // 'dragons'
  *   plural('dragons') // 'dragons'
  */
-export const plural = string =>
-  string.endsWith('s') === false ? `${string}s` : string
+export const plural = string => {
+  if (string.endsWith('s') === false) {
+    // @ts-ignore
+    return `${string}s`
+  }
+  // @ts-ignore
+  return `${string}`
+}
